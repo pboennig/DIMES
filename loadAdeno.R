c7084bae94c3bc5ccce5eea2a5984e22ea9b8a31
pboennig <pboennig@gmail.com>
2020-05-14 00:08:41 -0700

    Wrote ppi comparison script as well as util script to load adeno dataset

---
+++ loadAdeno.R
@@ -0,0 +1,16 @@
+library(Seurat)
+library(readxl)
+load("./adenoallraw.RData")
+type <- "tumor"
+lung <- CreateSeuratObject(adeno.all$TUMOR, min.cells = 3, min.features = 0, project = "nsclc")
+metadata <- read_excel("./MetaData.xlsx", sheet = 1)
+strsplit.ind2 <- seq(from=2, by=2, length.out = nrow(metadata))
+metadata <- metadata %>%
+    mutate(piece = unlist(strsplit(Patient_piece, split="-"))[strsplit.ind2]) %>%
+    filter(CellType == type)
+lung <- AddMetaData(object = lung, metadata = metadata)
+lung <- NormalizeData(object = lung, normalization.method = "LogNormalize", scale.factor = 1e4)
+all.genes <- rownames(x = lung)
+lung <- ScaleData(object = lung, features = all.genes)
+cells <- metadata[metadata$'PatientNumber MS' %in% c(3:4),]$cell
+rm(list = c("metadata", "strsplit.ind2", "all.genes"))
---
+++ ppi_comp.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(STRINGdb)
+library(igraph)
+source("dimess.R")
+
+source("loadAdeno.R") #load adeno data
+
+metrics <- c('pearson', 'spearman', 'kendall', 'bicor', 'binomial', 'MI',
+  'cosine', 'jaccard', 'canberra', 'euclidean', 'manhattan',
+  'weighted_rank', 'hamming')
+graphs <- build_gene_graphs(lung, cells, metrics)
+
+string_db <- STRINGdb$new(version="11")
+mapped <- graphs[[1]] %>% 
+            activate(nodes) %>% 
+            as.data.frame %>%
+            string_db$map("name", removeUnmappedRows = TRUE)
+
+string_subset <- string_db$get_subnetwork(mapped$STRING_id) %>% as_adjacency_matrix
+calc_acc <- function(m, graphs, mapped, string_subset) {
+  num_connections <- sum(string_subset / 2) # how many PPI connections
+  adj_mat <- as.igraph(graphs[[m]]) %>%
+          simplify %>% # remove self-edges
+          delete.vertices(setdiff(V(.)$name, mapped$name)) %>%
+          set.vertex.attribute("name", value = mapped[which(mapped$name == V(.)$name),]$STRING_id) %>%
+          delete.edges(E(.)[E(.)$weight > sort(E(.)$weight)[num_connections]]) %>%
+          as_adjacency_matrix
+  1 - norm(adj_mat - string_subset / 2) / num_connections
+}
+accs <- lapply(metrics, calc_acc, graphs, mapped, string_subset)
+names(accs) <- metrics
