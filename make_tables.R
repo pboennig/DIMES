07ff6bf1e3368145ef32432e3f6c5086a10067b7
pboennig <pboennig@gmail.com>
2020-06-29 22:29:10 -0700

    Update table formatting to use gt

--- make_tables.R
+++ make_tables.R
@@ -1,26 +1,11 @@
-library("formattable")
+library("gt")
 library("dplyr")
+library("purrr")
+library("glue")
 library("htmltools")
 library("webshot")
 
-export_formattable <- function(f, file, width = "100%", height = NULL, 
-                               background = "white", delay = 0.2)
-{
-  w <- as.htmlwidget(f, width = width, height = height)
-  path <- html_print(w, background = background, viewer = NULL)
-  url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
-  webshot(url,
-          file = file,
-          selector = ".formattable_widget",
-          delay = delay)
-}
-
-get_df <- function(cell_line) {
-  df <- paste("aucs/", cell_line, ".csv", sep="") %>% read.csv
-  df[3] <- round(df[3], 2)
-  df[,-1]
-}
-make_tables <- function(cell_lines) {
+make_tables <- function(cancer_type, cell_lines) {
   df <- get_df(cell_lines[1])
   colnames(df)[2] <- cell_lines[1]
   if (length(cell_lines) > 1) {
@@ -28,16 +13,14 @@ make_tables <- function(cell_lines) {
       df[, cell_lines[i]] <- get_df(cell_lines[i])$auc
     }
   }
-  alignment <- c("l", rep("c", length(cell_lines)))
-  formattable(df, align=alignment)
+  gt(df) %>% 
+    tab_header(title = cancer_type) %>% 
+    gtsave(glue("aucs/{cancer_type}.png"))
 }
 
-breast <- c("MCF7", "SUM159")
-export_formattable(make_tables(breast), "aucs/breast.png", width="25%")
-liver <- c("HUH1", "HUH7")
-export_formattable(make_tables(liver), "aucs/liver.png", width="25%")
-crc <- c("A549", "H1437", "HCT116", "IMR90", "GM12878", "H1")
-export_formattable(make_tables(crc), "aucs/crc.png", width="50%")
-leukemia <- c("K652")
-export_formattable(make_tables(leukemia), "aucs/leukemia.png", width="50%")
-
+cellLineMap <- list()
+cellLineMap[["Breast"]] <- c("MCF7", "SUM159")
+cellLineMap[["Liver"]] <- c("HUH1", "HUH7")
+cellLineMap[["CRC"]] <- c("A549", "H1437", "HCT116", "IMR90", "GM12878", "H1")
+cellLineMap[["Leukemia"]] <- c("K652")
+map2(names(cellLineMap), cellLineMap, make_tables)
