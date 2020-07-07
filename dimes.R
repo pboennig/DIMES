34e30a466f357ae6e124731ba075363c791a9f08
pboennig <pboennig@gmail.com>
2020-07-06 22:30:27 -0700

    Run on full datasets
    reorg files
    
    run on full datasets
    
    new git ignore

--- dimes.R
+++ dimes.R
@@ -12,18 +12,23 @@ library(WGCNA)
 library(STRINGdb)
 library(igraph)
 library(plotROC)
+library(glue)
 
 #' Calculates the AUCs for different *metrics* on a particular Seurat *obj* by comparing
 #' to STRINGdb network
-ppi_comp <- function(obj, cells=colnames(obj), num_genes=200, metrics=c('pearson', 'spearman', 'kendall', 'bicor', 'binomial', 'MI',
+ppi_comp <- function(obj, cells=colnames(obj), num_genes= nrow(obj), metrics=c('pearson', 'spearman', 'kendall', 'bicor', 'binomial', 'MI',
                                                           'cosine', 'jaccard', 'canberra', 'euclidean', 'manhattan',
                                                           'weighted_rank', 'hamming', 'rho_p', 'phi_s')) {
-  genes <- data.frame(gene=head(VariableFeatures(obj), num_genes)) 
+  if (num_genes < nrow(obj)) {
+    genes <- data.frame(gene=head(VariableFeatures(obj), num_genes)) 
+  } else {
+    genes <-data.frame(gene=VariableFeatures(obj))
+  }
   string_db <- STRINGdb$new(version="11", score_threshold=50, input_directory="STRINGdb/", species=9606)
   mapped <- string_db$map(genes, "gene", removeUnmappedRows=TRUE)
   graphs <- build_gene_graphs(obj, cells, mapped, metrics)
   # filter out genes that didn't appear in assayData 
-  
+  print("DIMES: Graphs built")  
   # mutinfo functions remove '-' characters
   if ('MI' %in% metrics) {
     pears_names <- graphs$pearson %>% pull(name)
@@ -32,17 +37,23 @@ ppi_comp <- function(obj, cells=colnames(obj), num_genes=200, metrics=c('pearson
   
   string_subset <- string_db$get_subnetwork(mapped$STRING_id)
   string_subset <- delete.edges(string_subset, which(E(string_subset)$experiments < 400)) %>%
-                   as_adjacency_matrix
+                   as_adjacency_matrix %>% as.matrix
   string_subset <- string_subset / 2# adjacency matrix has 2 if edge exists, want it to be 0/1
   scores <- lapply(graphs, calc_score, mapped, string_subset)
+  rm(graphs) # no longer need graphs (they take up a bunch of memory)
+  print("DIMES: Scores calculated")  
   
+  string_subset <- string_subset[rownames(scores[[1]]), ]
+  string_subset <- string_subset[, colnames(scores[[1]])]
   names(scores) <- metrics
-  s <- data.frame(scores)
+  s <- data.frame(lapply(scores, as.vector))
   s$status <- as.vector(string_subset) # score matrix w/ labels in status column
   longtest <- melt_roc(s, d = "status", m = metrics) # merge into long data (all metrics together in one column)
   p <- ggplot(longtest, aes(d = D, m = M, color = name)) + geom_roc(labels=FALSE, increasing=FALSE) + style_roc() # plot ROCs
   aucs <- data.frame(metric=metrics, auc=calc_auc(p)$AUC)
-  rm(list=setdiff(ls(), "aucs"))
+  print("DIMES: Cleaning up...")
+  rm(list=c("string_subset", "scores", "mapped", "string_db")) 
+  gc()
   aucs
 }
 
@@ -54,7 +65,7 @@ calc_score <- function(g, mapped, string_subset) {
   mapped_string_ids <- intersect(rownames(string_subset), rownames(score))
   correspondence <- match(mapped_string_ids, rownames(score))
   score <- score[correspondence,]
-  score[,correspondence] %>% as.vector 
+  score[,correspondence]
 }
 
 
@@ -76,13 +87,17 @@ build_gene_graphs <- function(obj, cells, mapped, metrics) {
 }
 
 #' Wrapper around dismay, switch on metrics that need raw counts
-dist_matrix <-function(metric, count_mat, log_mat) {
+dist_matrix <-function(metric, count_mat, log_mat, eps=.001) {
     if (metric %in% c('phi_s', 'rho_p')) {
         sim_mat <- dismay(count_mat, metric) 
     } else {
         sim_mat <- dismay(log_mat, metric) 
     }
-    as_tbl_graph(sim_mat)
+    sim_mat <- sim_mat - min(sim_mat) + eps # make all positive with eps for numerical stability
+    d_mat <- 1 / (sim_mat) # convert to distance matrix
+    diag(d_mat) <- 0 # distance from gene to itself = 0
+    print(glue("DIMES: built graph for {metric}"))
+    as_tbl_graph(d_mat)
 }
 
 # 2. Functions for saving and visualizing networks ---------------------------------------------------
---
+++ load_scripts/load_adeno.R
@@ -0,0 +1,20 @@
+library(Seurat)
+library(readxl)
+source("../dimes.R")
+load("./adenoallraw.RData")
+type <- "tumor"
+lung <- CreateSeuratObject(adeno.all$TUMOR, min.cells = 3, min.features = 200, project = "nsclc")
+metadata <- read_excel("./MetaData.xlsx", sheet = 1)
+strsplit.ind2 <- seq(from=2, by=2, length.out = nrow(metadata))
+metadata <- metadata %>%
+    mutate(piece = unlist(strsplit(Patient_piece, split="-"))[strsplit.ind2]) %>%
+    filter(CellType == type)
+lung <- AddMetaData(object = lung, metadata = metadata)
+lung <- NormalizeData(object = lung, normalization.method = "LogNormalize", scale.factor = 1e4)
+lung <- FindVariableFeatures(lung)
+all.genes <- rownames(x = lung)
+lung <- ScaleData(object = lung, features = all.genes)
+adeno_cells <- metadata[metadata$'PatientNumber MS' %in% c(3:4),]$cell
+rm(list = c("metadata", "strsplit.ind2", "all.genes"))
+
+lung_aucs <- ppi_comp(lung, adeno_cells)
